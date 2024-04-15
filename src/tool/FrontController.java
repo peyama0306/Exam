21a57c332d25076bbe2f59646e2a3ec9c354f0e0
oohara <oohara@172.16.31.106>
2024-04-15 12:46:04 +0900

    4月15日までのプロジェクト

--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -12,22 +12,36 @@ import javax.servlet.http.HttpServletResponse;
 /**
  *Srvlet implementation class FrontController
  */
-@WebServlet("/FrontController")
+@WebServlet(urlPatterns ={"*.action"})
 public class FrontController extends HttpServlet {
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		try {
+			// パスを取得
+			String path = req.getServletPath().substring(1);
+			// ファイル名を取得しクラス名に変換
+			String name = path.replace("a", "A").replace('/', '.');
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		///TODO Auto-generated method stub
-		doGet(request,response);
+			System.out.println("★ servlet path -> " + req.getServletPath());
+			System.out.println("★ class name ->" + name);
+
+			// アクションクラスのインスタンスを返却
+			Action action = (Action) class.forName(name).getDeclaredConstructor().newInstance();
+
+			// 遷移先URLを取得
+			action.execute(req, res);
+			// String url = action.execute(req, res);
+			// req.getRequestDispatcher(url).forward(req, res);
+		} catch (Exception e) {
+			e.printStackTrace();
+			// エラーページへリダイレクト
+			req.getRequestDispatcher("/error.jsp").forward(req, res);
+		}
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+			doGet(req,res);
 	}
 }
